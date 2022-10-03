9bd472cfaed3a901957a580364ea6d4ef98809a0
Teun0n <simon.a.andreasen@gmail.com>
2022-10-03 21:09:21 +0200

    jol

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-#include<malloc.h>
+#include <malloc.h>
 #include "circle.h"
 #include"jollyjumper.h"
+#include <stdbool.h>
+#include <assert.h>
 
 void printCircle(circle c) {
 	printf("p.x: %d, p.y: %d, r: %d\n", c.p.x, c.p.y, c.r);
@@ -23,12 +25,17 @@ int main(void) {
 
 	/*answer to exercise 7.b*/
 	int n; /*number of numbers to read*/
+
+    scanf("%d",&n);
+    assert(n>0);
 	/*readin n and check that is is OK*/
 
-	int *numbers = malloc(sizeof(int) * n); /*the numbers read*/
+	int *numbers = malloc(sizeof(int) * n);
 
 	/*readin the n numbers in the array numbers*/
+    for (int i = 0; i < n; i++)
 
+     scanf("%d",&numbers[i]);
 	if (isJollyJumber(numbers, n)) {
 		printf("it is a Jolly Jumper");}
 	else {
