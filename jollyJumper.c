0b310ee4624971a47b0d2228e17ed042fd043405
Teun0n <simon.a.andreasen@gmail.com>
2022-10-03 20:36:43 +0200

    jolly

--- jollyJumper.c
+++ jollyJumper.c
@@ -13,17 +13,27 @@ int isJollyJumber(const int seq[], int size)
 bool diffs_found[size];
 diffs_found[0]=true;
 
-for(int j=1;j<size,j++)
+for(int j=1;j<size;j++)
 {
 diffs_found[j]=false;
 }
-for(int i=0;i<size, i++)
+
+
+for(int i=0;i<size;i++)
 {
 int num = 0;
-num = seq[i]-seq[i+1]
-if (abs(num)<=size)
+num = seq[i]-seq[i+1];
+if(abs(num)<=size)
+{
 diffs_found[abs(num)]=true;
-for(int k =0;k<size;j++)
+}
+}
+for(int k = 0;k<size;k++)
+{
+if (diffs_found[k]==false)
+
+return 0;
+
 }
 
     return 1;
