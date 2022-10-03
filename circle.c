3677bb970d754f7a5afb391267c4e026399d6746
Teun0n <simon.a.andreasen@gmail.com>
2022-10-03 14:38:44 +0200

    fix

--- circle.c
+++ circle.c
@@ -1,5 +1,7 @@
+#include <stdio.h>
 #include <malloc.h>
 #include "circle.h"
+#include <stdbool.h>
 
 circle c[5];
 
@@ -33,6 +35,25 @@ c->p.x = (c->p.x) + (p->x);
 c->p.y = (c->p.y) + (p->y);
 
 }
+void printCircle(circle c) {
+	printf("p.x: %d, p.y: %d, r: %d\n", c.p.x, c.p.y, c.r);
+}
 
+int main(void) {
+	/*testing circles - make sure that you know how it works and what it is supposed to print out*/
+	circle c[5];
+	fiveCircles(c);
+	for (int i = 0; i < 5; i++) {
+		printCircle(c[i]);
+	}
+	point p;
+	p.x = 2;
+	p.y = 2;
+	translate(&c[1], &p);
+	printCircle(c[1]);
+	printf("isValid: %d", circleIsValid(&c[1]));
+
+	return 0;
+}
 
 
