b711bdd36814de226d55eba3c84e51fa12981b0d
Teun0n <simon.a.andreasen@gmail.com>
2022-10-03 16:13:37 +0200

    bruh

--- jollyJumper.c
+++ jollyJumper.c
@@ -1,11 +1,31 @@
 #include "jollyjumper.h"
 #include <stdbool.h>
 #include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
 
-int isJollyJumber(const int seq[], int size) {
-/*post: answer to exercise 7.a*/
+
+int isJollyJumber(const int seq[], int size) 
+{
+
+bool diffs_found[size];
+diffs_found[0]=true;
+
+for(int j=1;j<size,j++)
+{
+diffs_found[j]=false;
+}
+for(int i=0;i<size, i++)
+{
+int num = 0;
+num = seq[i]-seq[i+1]
+if (abs(num)<=size)
+diffs_found[abs(num)]=true;
+for(int k =0;k<size;j++)
+}
+
     return 1;
 }
 
