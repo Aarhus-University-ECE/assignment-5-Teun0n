4b5d30b24aa0c2cf98966d7a5ba54683f9129a0a
Teun0n <simon.a.andreasen@gmail.com>
2022-10-03 14:56:08 +0200

    skirt

--- circle.c
+++ circle.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
 #include <malloc.h>
 #include "circle.h"
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-circle c[5];
+
 
 void fiveCircles(circle c[]) {
 for(int i = 0; i<5; i++){
@@ -11,9 +12,8 @@ for(int i = 0; i<5; i++){
 c[i].p.x = i;
 c[i].p.y = i;
 c[i].r = i;
-
 }
-
+circle c[5];
 
 }
 
@@ -35,25 +35,3 @@ c->p.x = (c->p.x) + (p->x);
 c->p.y = (c->p.y) + (p->y);
 
 }
-void printCircle(circle c) {
-	printf("p.x: %d, p.y: %d, r: %d\n", c.p.x, c.p.y, c.r);
-}
-
-int main(void) {
-	/*testing circles - make sure that you know how it works and what it is supposed to print out*/
-	circle c[5];
-	fiveCircles(c);
-	for (int i = 0; i < 5; i++) {
-		printCircle(c[i]);
-	}
-	point p;
-	p.x = 2;
-	p.y = 2;
-	translate(&c[1], &p);
-	printCircle(c[1]);
-	printf("isValid: %d", circleIsValid(&c[1]));
-
-	return 0;
-}
-
-
